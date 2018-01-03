b3383675147fea51ef4a3c5b3a7bd4341a613992
unknown <nuxlstep@gmail.com>
2018-01-03 13:33:32 +0700

    Initial...

--- index.php
+++ index.php
@@ -1,20 +1,13 @@
-<?PHP
+<?php
+/**
+ * Use for return easy answer.
+ */
 require_once('./vendor/autoload.php');
 use \LINE\LINEBot\HTTPClient\CurlHTTPClient;
 use \LINE\LINEBot;
 use \LINE\LINEBot\MessageBuilder\TextMessageBuilder;
-// Token
 $channel_token = 'ohm7HGbyR8rqmNuryddTXETn18dFngQo2Xy1VnJL27E0KJB6BbKjj0F8hgNCjdMkAIb2wBoQpuayS33w1XW61HIKt1F76IsjFQU5T5d1fZUVKNpgJ36UUw/YFy87VXdkqLLoffTe53BJU3BHQXJ68QdB04t89/1O/w1cDnyilFU='; 
-$channel_secret = 'c70f830212cdded6367ad678cce5d73f'; 
-// Create bot
-$httpClient = new CurlHTTPClient($channel_token);
-$bot = new LINEBot($httpClient, array('channelSecret' => $channel_secret));
-// Database connection 
- $host = 'ec2-50-17-250-30.compute-1.amazonaws.com';
-                $dbname = 'dbvl9ckm0gjlci';
-                $user = 'jtmypzcqbwwcuf';
-                $pass = 'cce251fff9748a8c0205def179b6979aec503f8e67e656a056fd2a354d57181d';
-                $connection = new PDO("pgsql:host=$host;dbname=$dbname", $user, $pass); 
+$channel_secret = 'c70f830212cdded6367ad678cce5d73f';
 // Get message from Line API
 $content = file_get_contents('php://input');
 $events = json_decode($content, true);
@@ -23,84 +16,31 @@ if (!is_null($events['events'])) {
 	foreach ($events['events'] as $event) {
     
         // Line API send a lot of event type, we interested in message only.
-		if ($event['type'] == 'message') {
-            switch($event['message']['type']) {
-                case 'text':
-                    
-                    $sql = sprintf(
-                        "SELECT * FROM slips WHERE slip_date='%s' AND user_id='%s' ", 
-                        date('Y-m-d'),
-                        $event['source']['userId']);
-                    $result = $connection->query($sql);
-                    if($result !== false && $result->rowCount() >0) {
-                        // Save database
-                        $params = array(
-                            'name' => $event['message']['text'],
-                            'slip_date' => date('Y-m-d'),
-                            'user_id' => $event['source']['userId'],
-                        );
-                        $statement = $connection->prepare('UPDATE slips SET name=:name WHERE slip_date=:slip_date AND user_id=:user_id'); 
-                        $statement->execute($params);
-                    } else {
-                        $params = array(
-                            'user_id' => $event['source']['userId'] ,
-                            'slip_date' => date('Y-m-d'),
-                            'name' => $event['message']['text'],
-                        );
-                        $statement = $connection->prepare('INSERT INTO slips (user_id, slip_date, name) VALUES (:user_id, :slip_date, :name)');
-                         
-                        $effect = $statement->execute($params);
-                    }
-                    // Bot response 
-                    $respMessage = 'Your data has saved.';
-                    $replyToken = $event['replyToken'];
-                    $textMessageBuilder = new TextMessageBuilder($respMessage);
-                    $response = $bot->replyMessage($replyToken, $textMessageBuilder);
+		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
+            // Get replyToken
+            $replyToken = $event['replyToken'];
+            switch($event['message']['text']) {
+                
+                case 'tel':
+                    $respMessage = '089-5124512';
                     break;
-                case 'image':
-                    // Get file content.
-                    $fileID = $event['message']['id'];
-                    
-                    $response = $bot->getMessageContent($fileID);
-                    $fileName = md5(date('Y-m-d')).'.jpg';
-                    
-                    if ($response->isSucceeded()) {
-                        // Create file.
-                        $file = fopen($fileName, 'w');
-                        fwrite($file, $response->getRawBody());
-                        $sql = sprintf(
-                                    "SELECT * FROM slips WHERE slip_date='%s' AND user_id='%s' ", 
-                                    date('Y-m-d'),
-                                    $event['source']['userId']);
-                        $result = $connection->query($sql);
-                        if($result !== false && $result->rowCount() >0) {
-                            // Save database
-                            $params = array(
-                                'image' => $fileName,
-                                'slip_date' => date('Y-m-d'),
-                                'user_id' => $event['source']['userId'],
-                            );
-                            $statement = $connection->prepare('UPDATE slips SET image=:image WHERE slip_date=:slip_date AND user_id=:user_id');
-                            $statement->execute($params);
-                            
-                        } else {
-                            $params = array(
-                                'user_id' => $event['source']['userId'] ,
-                                'image' => $fileName,
-                                'slip_date' => date('Y-m-d'),
-                            );
-                            $statement = $connection->prepare('INSERT INTO slips (user_id, image, slip_date) VALUES (:user_id, :image, :slip_date)');
-                            $statement->execute($params);
-                        }
-                    }
-                    // Bot response 
-                    $respMessage = 'Your data has saved.';
-                    $replyToken = $event['replyToken'];
-                    $textMessageBuilder = new TextMessageBuilder($respMessage);
-                    $response = $bot->replyMessage($replyToken, $textMessageBuilder);
-                    
-                    break; 
+                case 'address':
+                    $respMessage = '99/451 Muang Nonthaburi';
+                    break;
+                case 'boss':
+                    $respMessage = '089-2541545';
+                    break;
+                case 'idcard':
+                    $respMessage = '5845122451245';
+                    break;
+                default:
+                    break;
             }
+            $httpClient = new CurlHTTPClient($channel_token);
+            $bot = new LINEBot($httpClient, array('channelSecret' => $channel_secret));
+            $textMessageBuilder = new TextMessageBuilder($respMessage);
+            $response = $bot->replyMessage($replyToken, $textMessageBuilder);
 		}
 	}
 }
+echo "OK";

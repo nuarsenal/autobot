30e004afa5e0c649ee22fb54dde4429fefa5c2a6
unknown <nuxlstep@gmail.com>
2018-01-03 12:00:56 +0700

    Initial...

--- index.php
+++ index.php
@@ -4,8 +4,8 @@ require_once('./vendor/autoload.php');
 use \LINE\LINEBot\HTTPClient\CurlHTTPClient; 
 use \LINE\LINEBot; 
 use \LINE\LINEBot\MessageBuilder\TextMessageBuilder; 
-$channel_token = '1v2OUa9tuMIiDhEg57ANbsRaBDbBGP9nlCC+Dpvt5HrsQ+LqcrImWPUBkH8re/pwqxv56d15kZeMoU/vQ0zuzPFlbhFM7AhRMZwLrSkLdcjbFurwXGOyHLt8MdgzLfAe7r0BsQV5cATlUanW3OgJewdB04t89/1O/w1cDnyilFU='; 
-$channel_secret = '9b2c7349ea939ef723a3cb453d774c86'; 
+$channel_token = 'ohm7HGbyR8rqmNuryddTXETn18dFngQo2Xy1VnJL27E0KJB6BbKjj0F8hgNCjdMkAIb2wBoQpuayS33w1XW61HIKt1F76IsjFQU5T5d1fZUVKNpgJ36UUw/YFy87VXdkqLLoffTe53BJU3BHQXJ68QdB04t89/1O/w1cDnyilFU='; 
+$channel_secret = 'c70f830212cdded6367ad678cce5d73f'; 
 // Get message from Line API 
 $content = file_get_contents('php://input'); 
 $events = json_decode($content, true); 
@@ -16,16 +16,17 @@ if (!is_null($events['events'])) {
 		if ($event['type'] == 'message')
 				 { 
 				 	switch($event['message']['type']) 
-							{ case 'text': 
-						// Get replyToken 
-						$replyToken = $event['replyToken']; 
-						// Reply message 
-						$respMessage = 'Hello, your message is '. $event['message']['text']; $httpClient = new CurlHTTPClient($channel_token); $bot = new LINEBot($httpClient, array('channelSecret' => $channel_secret)); $textMessageBuilder = new TextMessageBuilder($respMessage); $response = $bot->replyMessage($replyToken, $textMessageBuilder); break; 
+							{ 
+								case 'text': 
+										// Get replyToken 
+										$replyToken = $event['replyToken']; 
+										// Reply message 
+										$respMessage = 'Hello, your message is '. $event['message']['text']; $httpClient = new CurlHTTPClient($channel_token); $bot = new LINEBot($httpClient, array('channelSecret' => $channel_secret)); $textMessageBuilder = new TextMessageBuilder($respMessage); $response = $bot->replyMessage($replyToken, $textMessageBuilder); break; 
 						}
 				}
 		}
 }
 
-echo "ok";
-echo "<br> ทดสอบ Bot";
+//echo "ok";
+//echo "<br> ทดสอบ Bot";
 
